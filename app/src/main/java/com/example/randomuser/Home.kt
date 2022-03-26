1de5329e31db5e5552c8e63e5b4bd1e82e9a6777
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-03-26 12:32:26 +0100

    add basic navigation and empty Users composable

--- app/src/main/java/com/example/randomuser/Home.kt
+++ app/src/main/java/com/example/randomuser/Home.kt
@@ -1,8 +1,28 @@
 package com.example.randomuser
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.example.randomuser.users.Users
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun Home() {
+    val navController = rememberAnimatedNavController()
+
+    Scaffold {
+        AnimatedNavHost(
+            navController = navController,
+            startDestination = "users"
+        ) {
+            composable("users") {
+                Users()
+            }
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/example/randomuser/users/Users.kt
@@ -0,0 +1,13 @@
+package com.example.randomuser.users
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Users() {
+    LazyColumn {
+
+    }
+}
+
+
