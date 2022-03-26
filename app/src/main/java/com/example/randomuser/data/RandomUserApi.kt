239403422a634b8dc6002e82a164ccd70881285c
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-03-26 13:08:52 +0100

    add response models

--- app/src/main/java/com/example/randomuser/data/RandomUserApi.kt
+++ app/src/main/java/com/example/randomuser/data/RandomUserApi.kt
@@ -1,6 +1,12 @@
 package com.example.randomuser.data
 
+import UserResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface RandomUserApi {
 
+    @GET("api")
+    suspend fun getUsers(@Path("results") resultSize: Int): UserResponse
 
 }
